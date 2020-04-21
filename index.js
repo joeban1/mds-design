2254a8207dd63ddf58e4583a5163dd0b4385ff6b
joeban1 <45437007+joeban1@users.noreply.github.com>
2020-04-21 23:04:35 +0800

    first pass - responsiveness

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
-$(document).ready(function()
+$(document).ready(function ()
 
-{
-    $("#icey1").hover(
-        function()
-        {
-            $(this).attr("src", "images/icehouse.gif");
-        },
-        function()
-        {
-            $(this).attr("src", "images/icehouse-static.jpg");
-        }                         
-    ); 
-    
-    $("#icey2").hover(
-        function()
-        {
-            $(this).attr("src", "images/Selva.gif");
-        },
-        function()
-        {
-            $(this).attr("src", "images/proj2.jpg");
-        }                         
-    ); 
-        
-});
+    {
+        $("#icey1").hover(
+            function () {
+                $(this).attr("src", "images/icehouse.gif");
+            },
+            function () {
+                $(this).attr("src", "images/icehouse-static.jpg");
+            }
+        );
 
-/*! Fades out the whole page when clicking links */
-$('a').click(function(e) {
-    e.preventDefault();
-    newLocation = this.href;
-    $('body').fadeOut('slow', newpage);
-    });
-    function newpage() {
-    window.location = newLocation;
-    }
-
-    $(document).ready(function(){
+        $("#icey2").hover(
+            function () {
+                $(this).attr("src", "images/Selva.gif");
+            },
+            function () {
+                $(this).attr("src", "images/proj2.jpg");
+            }
+        );
 
         /*! Fades in whole page on load */
         $('body').css('display', 'none');
-        $('body').fadeIn(500);
-        
-        }); 
+        $('body').fadeIn(1000);
+
+
+    });
+
+/*! Fades out the whole page when clicking links */
+$('a').click(function (e) {
+    e.preventDefault();
+    newLocation = this.href;
+    $('body').fadeOut('slow', newpage);
+});
+
+function newpage() {
+    window.location = newLocation;
+}
+
+$(document).ready(function () {
+
+
+});
